54a78dcc78cfcc8cd14ab6cb9c31f89e64bf3f4b
Carlos Castillo <carlos@diosplan.com>
2025-04-22 11:03:39 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
             <nav className="flex justify-end grow sm:gap-2">
               <a
                 className="flex gap-2 px-3 py-2 text-sm font-semibold text-gray-600 transition duration-100 rounded-md hover:text-gray-800"
-                href="https://chipisdk.mintlify.app/"
+                href="https://sdkdocs.chipipay.com/"
               >
                 <div className="m-auto">
                   <Docs />
